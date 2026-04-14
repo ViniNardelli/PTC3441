92828e7d74e26fe87b9a1ab62a9b350c4d4151af
ViniNardelli <64744642+ViniNardelli@users.noreply.github.com>
2026-04-14 00:25:06 -0300

    Exercise 02 - 'Lista de Simulação 1/+functions/tmult' implementation

---
+++ Lista de Simulação 1/+functions/tmult.m
@@ -0,0 +1,62 @@
+% TMULT
+% Realiza a multiplicação de duas matrizes de transformação homogênea
+% entre si:
+% ${}^A T_C = {}^A T_B * {}^B T_C$
+% 
+%% Calling Syntax
+% crela = tmult(brela, crelb)
+%
+%% I/O Variables
+% |IN Double Matrix| *brela*: Homogeneous Transformation Matrix 4x4
+%
+% |IN Double Matrix| *crelb*: Homogeneous Transformation Matrix 4x4
+% 
+% |OUT Double Matrix| *crela*: Homogeneous Transformation Matrix 4x4
+%
+%% Example
+%  brela = [0 -1 0 1;
+%           1  0 0 0;
+%           0  0 1 0;
+%           0  0 0 1]
+%  crelb = [0 -1 0 0;
+%           1  0 0 1;
+%           0  0 1 0;
+%           0  0 0 1]
+%  crela = tmult(brela, crelb)
+%
+% crela =
+% 
+%     -1     0     0     0
+%      0    -1     0     0
+%      0     0     1     0
+%      0     0     0     1
+%
+% For more examples, see "../tests/tmultTest"
+%
+%% Hypothesis
+% RRR planar robot.
+%
+%% Limitations
+% MatLab version: >= R2023a
+%
+%% Version Control
+%
+% 1.0; Vicente Servio Saraiva Orrú, Vinícius Nardelli Santiago; 2026/04/13;
+% First issue.
+% 
+     
+%% Function
+function [crela] = tmult(brela, crelb)
+%% Validity
+    arguments (Input)
+        brela {functions.mustBeHomTransfR}
+        crelb {functions.mustBeHomTransfR}
+    end
+
+    arguments (Output)
+        crela {functions.mustBeHomTransfR}
+    end
+    
+%% Main Calculations & Output Data
+    crela = brela * crelb;
+end
